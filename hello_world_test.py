bda9e845c1f8952c6d8dd8a299c725c18587af63
Hunter Vevia <HunterJV2004@gmail.com>
2024-06-11 11:22:32 -0400

    M01 Programming Assignment - Numbers and Types

--- hello_world_test.py
+++ hello_world_test.py
@@ -1,1 +1,6 @@
-print("Hello World")
+seconds = 60
+minutes = 60
+seconds_per_hour = seconds * minutes
+seconds_per_day = seconds_per_hour * 24
+print("floating point division - " + str(seconds_per_day/seconds_per_hour)) ##Results are 24.0
+print("integer division - " + str(seconds_per_day//seconds_per_hour)) ##Results are 24
